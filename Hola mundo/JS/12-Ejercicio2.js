59f886be02174df2dc7ecd4c87bbfac4b4a825ca
unknown <jhonna.somarriba@gmail.com>
2022-03-11 17:07:37 -0600

    Se realizaron ejercicios1 y ejercicio2

---
+++ Hola mundo/JS/12-Ejercicio2.js
@@ -0,0 +1,27 @@
+'Use strict'
+//Mostrar la media y la suma de 2 resultados, introducidos
+//por el usuario, hasta que el usuario ingrese un # negativo
+
+var suma=0;
+var contador=0;
+
+do {
+    var numero=parseInt(prompt("Introduce # hasta que metas un # negativo",0));
+    if(isNaN(numero)){
+        numero=0;
+
+    }
+    else if(numero>=0){
+        //suma=+numero
+        suma=suma+numero;
+        
+        contador++;
+
+    }
+    console.log(suma);
+    console.log(contador);
+
+}while(numero>=0)
+
+alert("La suma de todos los numeros es: "+suma);
+alert("La media de todos los numeros es: "+(suma/contador));
