f2232a3be7a2b8a31f1008c4259bcb642c528a29
Jhonna Somarriba <jhonna.somarriba@gmail.com>
2022-03-10 22:52:28 -0600

    Se realiza ejercicio de tipos de ventanas en JS

---
+++ Hola mundo/JS/10-Ventanas.js
@@ -0,0 +1,13 @@
+'Use strict'
+//Alerta
+alert("Esta es mi alerta");
+alert("Esta es mi texto");
+
+//Alertas de confirmacion
+var mi_resultado=confirm("Estas seguro de querer continuar");
+console.log(mi_resultado);
+
+//Ventana de ingreso de datos
+var mi_result=prompt("Que edad tienes?",18);
+console.log(typeof mi_result);
+
