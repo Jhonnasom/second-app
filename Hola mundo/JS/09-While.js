309636695927406a626cc35ef865a047ff797997
Jhonna Somarriba <jhonna.somarriba@gmail.com>
2022-03-10 22:51:36 -0600

    Se realiza ejercicio con While

---
+++ Hola mundo/JS/09-While.js
@@ -0,0 +1,21 @@
+'Use strict'
+//While
+var year=2018;
+while(year!=1991){
+    //Ejecuta esto
+    console.log("Estamos en el ano: "+year );
+
+    year--;
+}
+
+//Do While
+//Nos permite ejecutar primero un bloque de instrucciones
+//Luego la condicion
+
+var years=30;
+
+do{
+    alert("Solo cuando sea diferente a 20");
+    years--;
+
+}while(years >25)
