dceefe52f2963f1e8273fadbaf8a08e2de0952f4
Jhonna Somarriba <jhonna.somarriba@gmail.com>
2022-03-28 22:42:30 -0600

    Trabajamos con Transformaciones de datos

---
+++ Hola mundo/JS/23-Funciones-texto.js
@@ -0,0 +1,10 @@
+'Use Strict'
+//Tranformaciones de datos
+
+var numero=444;
+var texto1= "Bienvenidos al curso";
+var texto2= "Es muy buen curso";
+
+//Metodos de busca indexoff o search
+var busqueda=texto1.search("curso");
+console.log(busqueda);
