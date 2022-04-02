960e6cad06d156d6b35751794fe4fd43e9ae8ee1
Jhonna Somarriba <jhonna.somarriba@gmail.com>
2022-04-01 22:58:38 -0600

    Se realizaron cambios en el index, se trabajo con edicion de plantillas y uso de arrays

--- Hola mundo/JS/23-Funciones-texto.js
+++ Hola mundo/JS/23-Funciones-texto.js
@@ -2,9 +2,22 @@
 //Tranformaciones de datos
 
 var numero=444;
-var texto1= "Bienvenidos al curso";
-var texto2= "Es muy buen curso";
+var texto1 = "Bienvenidos al curso del curso";
+var texto2 = "Es muy buen curso";
 
 //Metodos de busca indexoff o search
-var busqueda=texto1.search("curso");
+//indexOff se obtiene la posicion de 
+//lastindexOff para encontrar la ultima apaicion de una palabra
+//Match es otra alternativa de busqueda
+//Substr para sacar la palabra que hay en un determinado array
+//starWith busca al inicio del String
+//endsWith busca al final del String
+//Includes busca en un string una palabra si 
+//la encuentra es true y sino false
+//Replace reemplaza un texto por otro
+//Slice solamente sirve para recortar un string
+//Split corta todos los parrafos
+//Trim quita los espacios sobrantes por delante y por detras
+
+var busqueda=texto1.trim();
 console.log(busqueda);
---
+++ Hola mundo/JS/25-Arrays.js
@@ -0,0 +1,10 @@
+'Use Strict'
+//Arrays, arreglos , matrices
+
+var nombre="Victor Robles";
+var nombres= ["Victor Robles","Juan Lopez","Manolo Garcia","Jose Martinez"];
+var lenguajes=new Array("PHP","JS","GO","JAVA");
+
+
+console.log(nombres[3]);
+console.log(lenguajes);
