4a097cd4983c0d51dcf2af7a2c7307a3a1e89e24
Jhonna Somarriba <jhonna.somarriba@gmail.com>
2022-04-05 22:48:41 -0600

    Se realizaron practicas de tipos de array con foreach

--- Hola mundo/JS/25-Arrays.js
+++ Hola mundo/JS/25-Arrays.js
@@ -3,8 +3,30 @@
 
 var nombre="Victor Robles";
 var nombres= ["Victor Robles","Juan Lopez","Manolo Garcia","Jose Martinez"];
-var lenguajes=new Array("PHP","JS","GO","JAVA");
 
+var lenguajes=new Array("PHP","JS","GO","JAVA","C#");
+/*
+var elemento=parseInt(prompt("Que elemento del array quieres?",0));
+alert(nombres[elemento]);
 
-console.log(nombres[3]);
-console.log(lenguajes);
+if(elemento>=nombres.length){
+    alert("Introduce el numero correcto menor que "+nombres.length);
+}
+else{
+    alert("El usuario seleccionado es: "+nombres[elemento]);
+}
+*/
+
+document.write("<H1>Lenguajes de programacion del 2018</H1>");
+document.write("<ul>");
+/*
+for(var i=0;i<lenguajes.length;i++){
+    document.write("<li>"+lenguajes[i]+"</li>");
+}*/
+
+lenguajes.forEach((elemento,indice,array)=>{
+    console.log(Array);
+    document.write("<li>"+indice+" - "+elemento+" </li");
+
+});
+document.write("</ul>");
