8f25a2cfcd5a4a572a86c39dcdc6024fe65a1768
Jhonna Somarriba <jhonna.somarriba@gmail.com>
2022-04-19 23:37:31 -0600

    Se esta trabajando en ejercicios con arrays

---
+++ Hola mundo/JS/27-Ejercicio-arrays.js
@@ -0,0 +1,16 @@
+'Use Strict'
+/* 
+1-Realiozar un programa que pida 6 numeros por pantalla y los ingrese en un
+    array
+2-Mostral el array entero (todos sus elementos) en el cuerpo de la pagina 
+    y la consola.
+3-Ordenarlos y mostrarlo.
+4-Invertir su orden y mostrarlo.
+5-Mostrar cuantos elementos tiene el array
+6-Busqueda de un valor introducido por el usuario que nos diga
+    si lo encuentra y su indice.
+*/
+var numeros=new Array(6);
+for(var i=0; i<5; i++){
+    numero[i]=
+}
