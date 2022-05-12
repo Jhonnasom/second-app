85a8f3c8344105015240ebc0f0bee93916ef003e
Jhonna Somarriba <jhonna.somarriba@gmail.com>
2022-05-11 22:49:01 -0600

    Introduccion a eventos

--- Hola mundo/JS/30-Eventos.js
+++ Hola mundo/JS/30-Eventos.js
@@ -1,17 +1,23 @@
 'Use Strict'
 //Eventos de raton
 
-var boton=document.querySelector("#boton");
 
 function cambiarColor(){
+    console.log("Me has dado click");
+
     var bg=boton.style.background;
+
     if(bg=="green"){
         boton.style.background="red";
-       
+   
     }else{
         boton.style.background="green";
     }
-    boton.style.padding="10px";
+
+    boton.style.paddig="10px";
     boton.style.border="1px solid #ccc";
     return true;
 }
+
+var boton=document.querySelector("#boton");
+boton.addEventListener('click',cambiarColor());
