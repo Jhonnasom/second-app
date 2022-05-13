14e9ec1e99d5b6bce8fc5c0841d3b31b2e069069
unknown <jhonna.somarriba@gmail.com>
2022-05-13 11:45:23 -0600

    Se realizaron pruebas con los eventos focus-blur-mouseout-mouseover-keypress-keydown

--- Hola mundo/JS/30-Eventos.js
+++ Hola mundo/JS/30-Eventos.js
@@ -14,10 +14,53 @@ function cambiarColor(){
         boton.style.background="green";
     }
 
-    boton.style.paddig="10px";
+    boton.style.padding="10px";
     boton.style.border="1px solid #ccc";
     return true;
 }
 
 var boton=document.querySelector("#boton");
-boton.addEventListener('click',cambiarColor());
+
+//Evento click
+boton.addEventListener('click',function(){
+    cambiarColor();
+});
+
+//Mouse over
+boton.addEventListener('mouseover',function(){
+    boton.style.background="#ccc";
+});
+
+//Mouseout
+boton.addEventListener('mouseout',function(){
+    boton.style.background="#ccc";
+});
+
+//Focus
+var input=document.querySelector("#campo_nombre");
+
+input.addEventListener('focus',function(){
+    console.log("[focus] Estas dentro del input");
+});
+
+//Blur
+input.addEventListener('blur',function(){
+    console.log("[blur] Estas fuera del input");
+});
+
+//Keydown
+input.addEventListener('keydown',function(Event){
+    console.log("[keydown] Pulsando ésta tecla",String.fromCharCode(Event.keyCode));
+});
+
+//Keypress
+input.addEventListener('keypress',function(Event){
+    console.log("[keypress] Tecla presionada",String.fromCharCode(Event.keyCode));
+});
+
+
+//Keyup
+input.addEventListener('keyup',function(Event){
+    console.log("[keyup] Tecla soltada",String.fromCharCode(Event.keyCode));
+});
+
