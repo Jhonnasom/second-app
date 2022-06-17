9aaf22ea1fbb102a1cef0862aaa7df99e8365c28
Jhonna Somarriba <jhonna.somarriba@gmail.com>
2022-06-16 18:04:12 -0600

    Ejercicio con FETCH

---
+++ Hola mundo/JS/36-FETCH.js
@@ -0,0 +1,21 @@
+'Use Strict';
+
+//Fetch (ajax) y peticiones a servicios /api rest
+var div_usuarios=document.querySelector("#usuarios");
+var usuarios= [];
+
+fetch('https://reqres.in/api/users')
+    .then(data => data.json()) //Esto convierte la url en un objeto JSON
+    .then(users => {
+        usuarios=users.data; //Ya tenemos en data todos esos datos recogidos y en la var usuario los guardamos
+        console.log(usuarios);
+
+        usuarios.map((user,i)=>{
+            let nombre=document.createElement('h3');
+
+            nombre.innerHTML=i+"."+user.first_name+" "+ user.last_name;
+            
+            div_usuarios.appendChild(nombre);
+            document.querySelector(".loading").style.display='none';
+        });
+    });
