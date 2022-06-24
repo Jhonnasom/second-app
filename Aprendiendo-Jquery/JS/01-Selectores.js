518a68407082ed7bfef318131f9ca15150897c14
Jhonna Somarriba <jhonna.somarriba@gmail.com>
2022-06-23 21:32:53 -0600

    Selectores de etiqueta jQuery

--- Aprendiendo-Jquery/JS/01-Selectores.js
+++ Aprendiendo-Jquery/JS/01-Selectores.js
@@ -12,6 +12,29 @@ $(document).ready(function(){
 
     $("#verde").css("background","green")
                 .css("color","white");
+
+    //Selectores de clases
+    var mi_clase= $('.zebra').css("padding","5px");
+
+    $('.sin_borde').click(function(){
+        console.log("Click dado!!!");
+        $(this).addClass('zebra');
+    });
+
+    //Selectores de etiqueta
+    var parrafos=$('p').css("cursor","pointer");
+
+    //Pasos para remover una clase con un click
+    //hasClass busca si un elemento tiene una clase 
+    parrafos.click(function(){
+        var that= $(this);
+        if(!that.hasClass('grande')){
+            that.addClass("grande");
+        }else{
+            that.removeClass('grande');
+        }
+      
+    });
 });
 
 
