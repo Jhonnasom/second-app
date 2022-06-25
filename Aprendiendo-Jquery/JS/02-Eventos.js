f5c81ecf59ae6648bd530c83efde1d6158720f42
Jhonna Somarriba <jhonna.somarriba@gmail.com>
2022-06-24 21:11:28 -0600

    Eventos Mouse-Hover-Click-dblClick

---
+++ Aprendiendo-Jquery/JS/02-Eventos.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+    //MouseOver y MouseOut
+    var caja=$("#caja");
+    
+    /*
+    caja.mouseover(function(){
+        $(this).css("background","red");
+    })
+
+    caja.mouseout(function(){
+        $(this).css("background","green");
+    })
+    */
+
+    function cambiaRojo(){
+        $(this).css("background","red");
+    }
+
+    function cambiaVerde(){
+        $(this).css("background","green");
+    }
+
+    //Hover
+    caja.hover(cambiaRojo,cambiaVerde);
+
+    //Click-Doble click
+    caja.click(function(){
+        $(this).css("background","blue")
+                .css("color","white");
+    });
+
+    caja.dblclick(function(){
+        $(this).css("background","pink")
+                .css("color","blue");
+    });
+});
