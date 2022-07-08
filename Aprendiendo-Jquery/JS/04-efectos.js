8f24e1f6504967be2c527f1fe8c178998d67df46
unknown <jhonna.somarriba@gmail.com>
2022-07-08 11:43:27 -0600

    Efectos, animaciones y mas en jQuery

--- Aprendiendo-Jquery/JS/04-efectos.js
+++ Aprendiendo-Jquery/JS/04-efectos.js
@@ -2,25 +2,65 @@
 
 $(document).ready(function(){
 
+        var caja=$("#caja")
         $("#mostrar").hide();
 
         $("#mostrar").click(function(){
             $(this).hide();
             $("#ocultar").show();
 
-            $("#caja").slideDown('slow');    
+            caja.slideDown('slow');    
         });
 
         $("#ocultar").click(function(){
             $(this).hide();
             $("#mostrar").show();
 
-            $("#caja").slideUp('fslow',0);    
+            caja.slideUp('slow', function(){
+                console.log("Cartel Ocultar");
+            }); 
+            
+
         });
 
         $("#todoenuno").click(function(){
-            $("#caja").slideToggle('fast');
+            caja.slideToggle('fast');
         });
 
+        $("#animar").click(function(){
+            caja.animate({
+                marginLeft:'500px',
+                fontSize:'40px',
+                height:'100px',
+                      },'slow');
+
+            caja.animate({
+                        marginLeft:'500px',
+                        fontSize:'40px',
+                        height:'100px',
+                              },'slow')
+                              
+                .animate({
+                    borderRadius:'900px',
+                    marginTop:'80px',
+                },'slow')
+
+                .animate({
+                    borderRadius:'0px',
+                    marginLeft:'0px',
+                },'slow')
+
+                .animate({
+                    borderRadius:'100px',
+                    marginTop:'0px',
+                },'slow')
+
+                .animate({
+                    marginLeft:'500px',
+                    fontSize:'40px',
+                    height:'100px',
+                          },'slow')
+
+        });
 
 });
