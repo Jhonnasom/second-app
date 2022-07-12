23f2c07bf5e0d3a97a8a0b52d4623453f5961094
Jhonna Somarriba <jhonna.somarriba@gmail.com>
2022-07-11 21:56:19 -0600

    Ajax Load/Metodo Get

---
+++ Aprendiendo-Jquery/JS/05-ajax.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    
+    //Load
+    //$("#datos").load("https://reqres.in/");
+
+    //Get y Post
+
+    $.get("https://reqres.in/api/users?",{page: 2},function(response){
+
+        response.data.forEach((element,index) => {
+            $("#datos").append("<p>"+element.first_name+" "+element.last_name+"</p>");
+        });
+    });
+});
