ad37c1b15e82cbb0f24ac0031044e3fc665afa3d
unknown <jhonna.somarriba@gmail.com>
2022-07-13 13:22:34 -0600

    Interaciones de AJAX Draggable-Droppable-Resizable-Sortable-Selectable

--- Aprendiendo-Jquery/JS/06-interface.js
+++ Aprendiendo-Jquery/JS/06-interface.js
@@ -1,3 +1,28 @@
 $(document).ready(function(){
-    console.log("Todo en orden")
+
+    //Mover elemento por la pagina
+    $('.elemento').draggable();
+
+    //Redimensionar los elementos
+    $('.elemento').resizable();
+
+    //Seleccionar elementos
+    //$('.lista-seleccionable').selectable();
+
+    //Listar y ordenar los elementos
+    $('.lista-seleccionable').sortable({
+        update:function(event,ui){
+            console.log("Ha cambiado la lista");
+        }
+    });
+
+    //Drop
+    $("#elemento-movido").draggable();
+    $("#area").droppable({
+        drop:function(){
+            console.log("Has soltado algo dentro del area");
+        }
+    });
+
+
 });
