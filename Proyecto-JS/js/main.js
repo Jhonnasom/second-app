e7a1f34bab4d41cab251f1bb827730cd9ee1a348
unknown <jhonna.somarriba@gmail.com>
2022-07-20 14:33:51 -0600

    Selector de temas

--- Proyecto-JS/js/main.js
+++ Proyecto-JS/js/main.js
@@ -12,28 +12,68 @@ $(document).ready(function(){
       //Posts
       var posts=[
         {
-          title:"Prueba de titulo1",
-          date:'Publicado el día '+ moment().format("MMM dddd YYYY"),
+          title:"Prueba de titulo 1",
+          date:'Publicado el dia '+ moment().date()+ " de "+ moment().format("MMMM")+" del "+moment().format("YYYY"),
           content:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.'
         },
         {
-          title:"Prueba de titulo2",
-          date:new Date(),
+          title:"Prueba de titulo 2",
+          date:'Publicado el dia '+ moment().date()+ " de "+ moment().format("MMMM")+" del "+moment().format("YYYY"),
           content:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.'
         },
 
         {
-          title:"Prueba de titulo3",
-          date:new Date(),
+          title:"Prueba de titulo 3",
+          date:'Publicado el dia '+ moment().date()+ " de "+ moment().format("MMMM")+" del "+moment().format("YYYY"),
           content:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.'
         },
         {
-          title:"Prueba de titulo4",
-          date:new Date(),
+          title:"Prueba de titulo 4",
+          date:'Publicado el dia '+ moment().date()+ " de "+ moment().format("MMMM")+" del "+moment().format("YYYY"),
+          content:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.'
+        },
+
+        {
+          title:"Prueba de titulo 5",
+          date:'Publicado el dia '+ moment().date()+ " de "+ moment().format("MMMM")+" del "+moment().format("YYYY"),
+          content:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.'
+        },
+
+        {
+          title:"Prueba de titulo 6",
+          date:'Publicado el dia '+ moment().date()+ " de "+ moment().format("MMMM")+" del "+moment().format("YYYY"),
           content:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.'
         },
       ];
 
-      console.log(posts);
+      posts.forEach((item,index)=>{
+        var post=`
+        <article class="post">
+                        <h2>${item.title}</h2>
+                        <span class="date">${item.date}</span>
+                        <p>
+                            ${item.content}
+                        </p>
+                        <a href="#" class="button-more">Leer mas</a>
+                    </article>
+                    `;
+        $("#posts").append(post);
+      });
       
+      //Selector de temas
+      var theme=$("#theme");
+      
+      $("#to-green").click(function(){
+        theme.attr("href","css/green.css");
+      });
+
+      var theme=$("#theme");
+      $("#to-blue").click(function(){
+        theme.attr("href","css/blue.css");
+      });
+
+      var theme=$("#theme");
+      $("#to-red").click(function(){
+        theme.attr("href","css/red.css");
+      });
     });
