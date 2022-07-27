4d9e09fa9d88f7dffad04acce206566ccbbe586f
unknown <jhonna.somarriba@gmail.com>
2022-07-27 14:15:18 -0600

    Proyecto JS completado

--- Proyecto-JS/js/main.js
+++ Proyecto-JS/js/main.js
@@ -1,144 +1,158 @@
 $(document).ready(function(){
 
-   //Acordeon
-   if(window.location.href.indexOf('about') >-1){
-    $("#acordeon").accordion();
-  }
+	// Slider
+	if(window.location.href.indexOf('index') > -1){
+	  
+	  $('.galeria').bxSlider({
+	    mode: 'fade',
+	    captions: false,
+	    slideWidth: 1200,
+	    responsive: true,
+	    pager: true
+	  });
 
-      //Slider
-      if(window.location.href.indexOf('index')>-1){
-          $('.galeria').bxSlider({
-            mode: 'fade',
-            captions: true,
-            slideWidth:1200,
-            responsive:true  
-          });
-        }
-    
-
-      //Posts
-      if(window.location.href.indexOf('index')>-1){
-
-      var posts=[
-        {
-          title:"Prueba de titulo 1",
-          date:'Publicado el dia '+ moment().date()+ " de "+ moment().format("MMMM")+" del "+moment().format("YYYY"),
-          content:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.'
-        },
-        {
-          title:"Prueba de titulo 2",
-          date:'Publicado el dia '+ moment().date()+ " de "+ moment().format("MMMM")+" del "+moment().format("YYYY"),
-          content:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.'
-        },
-
-        {
-          title:"Prueba de titulo 3",
-          date:'Publicado el dia '+ moment().date()+ " de "+ moment().format("MMMM")+" del "+moment().format("YYYY"),
-          content:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.'
-        },
-        {
-          title:"Prueba de titulo 4",
-          date:'Publicado el dia '+ moment().date()+ " de "+ moment().format("MMMM")+" del "+moment().format("YYYY"),
-          content:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.'
-        },
-
-        {
-          title:"Prueba de titulo 5",
-          date:'Publicado el dia '+ moment().date()+ " de "+ moment().format("MMMM")+" del "+moment().format("YYYY"),
-          content:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.'
-        },
-
-        {
-          title:"Prueba de titulo 6",
-          date:'Publicado el dia '+ moment().date()+ " de "+ moment().format("MMMM")+" del "+moment().format("YYYY"),
-          content:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.'
-        },
-      ];
-      };
-     
-
-     /*posts.forEach((item,index)=>{
-        var post=`
-        <article class="post">
-                        <h2>${item.title}</h2>
-                        <span class="date">${item.date}</span>
-                        <p>
-                            ${item.content}
-                        </p>
-                        <a href="#" class="button-more">Leer mas</a>
-                    </article>
-                    `;
-        $("#posts").append(post);
-      });*/
-      
-      //Selector de temas
-      var theme=$("#theme");
-      
-      $("#to-green").click(function(){
-        theme.attr("href","css/green.css");
-      });
-
-      $("#to-blue").click(function(){
-        theme.attr("href","css/blue.css");
-      });
-
-      $("#to-red").click(function(){
-        theme.attr("href","css/red.css");
-      });
+	}
 
 
-      //Scroll arriba de la web
-      $('.subir').click(function(e){
-          e.preventDefault();
+	// Posts
+	if(window.location.href.indexOf('index') > -1){
+		var posts = [
+			{
+				title: 'Prueba de titulo 1',
+				date: 'Publicado el dia ' + moment().date() + " de " + moment().format("MMMM") + " del " + moment().format("YYYY"),
+				content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sit amet malesuada erat, ac ullamcorper justo. Fusce sapien nibh, tempor fermentum mauris ac, tincidunt maximus diam. Quisque bibendum sed dui sit amet euismod. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Suspendisse quam sem, scelerisque sit amet libero nec, congue blandit dolor. Aliquam a vehicula mi. Morbi id convallis dolor. Nulla eu libero nec nulla fermentum viverra quis at magna. Quisque rutrum augue nulla, bibendum viverra sapien viverra vel. Quisque malesuada ultrices felis eu porttitor.'
+			},
+			{
+				title: 'Prueba de titulo 2',
+				date: 'Publicado el dia ' + moment().date() + " de " + moment().format("MMMM") + " del " + moment().format("YYYY"),
+				content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sit amet malesuada erat, ac ullamcorper justo. Fusce sapien nibh, tempor fermentum mauris ac, tincidunt maximus diam. Quisque bibendum sed dui sit amet euismod. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Suspendisse quam sem, scelerisque sit amet libero nec, congue blandit dolor. Aliquam a vehicula mi. Morbi id convallis dolor. Nulla eu libero nec nulla fermentum viverra quis at magna. Quisque rutrum augue nulla, bibendum viverra sapien viverra vel. Quisque malesuada ultrices felis eu porttitor.'
+			},
+			{
+				title: 'Prueba de titulo 3',
+				date: 'Publicado el dia ' + moment().date() + " de " + moment().format("MMMM") + " del " + moment().format("YYYY"),
+				content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sit amet malesuada erat, ac ullamcorper justo. Fusce sapien nibh, tempor fermentum mauris ac, tincidunt maximus diam. Quisque bibendum sed dui sit amet euismod. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Suspendisse quam sem, scelerisque sit amet libero nec, congue blandit dolor. Aliquam a vehicula mi. Morbi id convallis dolor. Nulla eu libero nec nulla fermentum viverra quis at magna. Quisque rutrum augue nulla, bibendum viverra sapien viverra vel. Quisque malesuada ultrices felis eu porttitor.'
+			},
+			{
+				title: 'Prueba de titulo 4',
+				date: 'Publicado el dia ' + moment().date() + " de " + moment().format("MMMM") + " del " + moment().format("YYYY"),
+				content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sit amet malesuada erat, ac ullamcorper justo. Fusce sapien nibh, tempor fermentum mauris ac, tincidunt maximus diam. Quisque bibendum sed dui sit amet euismod. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Suspendisse quam sem, scelerisque sit amet libero nec, congue blandit dolor. Aliquam a vehicula mi. Morbi id convallis dolor. Nulla eu libero nec nulla fermentum viverra quis at magna. Quisque rutrum augue nulla, bibendum viverra sapien viverra vel. Quisque malesuada ultrices felis eu porttitor.'
+			},
+			{
+				title: 'Prueba de titulo 5',
+				date: 'Publicado el dia ' + moment().date() + " de " + moment().format("MMMM") + " del " + moment().format("YYYY"),
+				content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sit amet malesuada erat, ac ullamcorper justo. Fusce sapien nibh, tempor fermentum mauris ac, tincidunt maximus diam. Quisque bibendum sed dui sit amet euismod. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Suspendisse quam sem, scelerisque sit amet libero nec, congue blandit dolor. Aliquam a vehicula mi. Morbi id convallis dolor. Nulla eu libero nec nulla fermentum viverra quis at magna. Quisque rutrum augue nulla, bibendum viverra sapien viverra vel. Quisque malesuada ultrices felis eu porttitor.'
+			},
+			{
+				title: 'Prueba de titulo 6',
+				date: 'Publicado el dia ' + moment().date() + " de " + moment().format("MMMM") + " del " + moment().format("YYYY"),
+				content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sit amet malesuada erat, ac ullamcorper justo. Fusce sapien nibh, tempor fermentum mauris ac, tincidunt maximus diam. Quisque bibendum sed dui sit amet euismod. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Suspendisse quam sem, scelerisque sit amet libero nec, congue blandit dolor. Aliquam a vehicula mi. Morbi id convallis dolor. Nulla eu libero nec nulla fermentum viverra quis at magna. Quisque rutrum augue nulla, bibendum viverra sapien viverra vel. Quisque malesuada ultrices felis eu porttitor.'
+			},
+		];
 
-        $('html,body').animate({
-            scrollTop:0
-        },500);
+		posts.forEach((item, index) => {
+			var post = `
+				<article class="post">
+					<h2>${item.title}</h2>
+					<span class="date">${item.date}</span>
+					<p>
+						${item.content}
+					</p>
+					<a href="#" class="button-more">Leer más</a>
+				</article>
+			`;
 
-        return false;
-      });
+			$("#posts").append(post);
+		});
+	}
 
-      //Login Falso
-      $("#login form").submit(function(){
-          var form_name= $("#form_name").val();
-          localStorage.setItem("form_name",form_name);
-      });
 
-      var form_name=localStorage.getItem("form_name");
-      
-      if(form_name !=null && form_name!="undefined"){
+	// Selector de tema
+	var theme = $("#theme");
 
-        var about_parrafo=$("#about p");
-        $("#about p").html("<br><strong>Bienvenido, "+form_name+"</strong>");
-        about_parrafo.append("<a href='#' id='logout'>Cerrar Sesion</a>");
-        
-        $("#login").hide();
+	$("#to-green").click(function(){
+		theme.attr("href", "css/green.css");
+	});
+	
+	$("#to-red").click(function(){
+		theme.attr("href", "css/red.css");
+	});
+	
+	$("#to-blue").click(function(){
+		theme.attr("href", "css/blue.css");
+	});
 
-        $("#logout").click(function(){
-            localStorage.clear();
-            location.reload();
-        });
-      }
+	
+	// Scroll arriba de la web
+	$('.subir').click(function(e){
+		e.preventDefault();
 
-      /*//Acordeon
-      if(window.location.href.indexOf('about') >-1){
-      $("#acordeon").accordion();
-      }*/
+		$('html, body').animate({
+			scrollTop: 0
+		}, 500);
 
-      //Reloj
-      if(window.location.href.indexOf('reloj')>-1){
-        setInterval(function(){
-          var reloj=moment().format("hh:mm:ss a");
-          $('#reloj').html(reloj);
-        }, 1000);
-      
-      }
-      //Validation
-      if(window.location.href.indexOf('contact')>-1){
-        $("form input[name='date']").datepicker();
-        $.validate({
-            lang:'es'
-          });
-        }
-      
-    });
+		return false;
+	});
+
+	// Login falso
+
+	$("#login form").submit(function(){
+		var form_name = $("#form_name").val();
+
+		localStorage.setItem("form_name", form_name);
+
+	});
+
+	var form_name = localStorage.getItem("form_name");
+
+	if(form_name != null && form_name != "undefined"){
+		var about_parrafo = $("#about p");
+
+		about_parrafo.html("<br><strong>Bienvenido, "+form_name+"</strong> ");
+		about_parrafo.append("<a href='#' id='logout'>Cerrar sesión</a>");
+
+		$("#login").hide();
+
+		$("#logout").click(function(){
+			localStorage.clear();
+			location.reload();
+		});
+
+	}
+
+
+	// Acordeon
+
+	if(window.location.href.indexOf('about') > -1){
+		$("#acordeon").accordion();
+	}
+
+
+	// Reloj
+	if(window.location.href.indexOf('reloj') > -1){
+
+		setInterval(function(){
+				var reloj = moment().format("hh:mm:ss");
+				$('#reloj').html(reloj);
+		}, 1000);
+		
+	
+	}
+
+	// Validación
+	if(window.location.href.indexOf('contact') > -1){
+	
+		$("form input[name='date']").datepicker({
+			dateFormat: 'dd-mm-yy'
+		});
+
+		$.validate({
+		    lang: 'es',
+		    errorMessagePosition: 'top',
+		    scrollToTopOnError: true
+		});
+
+	}
+
+	
+
+});
