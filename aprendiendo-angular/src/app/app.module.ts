56dc8641144897fabadbb528288423a277072ba3
Jhonna Somarriba <jhonna.somarriba@gmail.com>
2022-08-08 22:47:31 -0600

    Componentes en Angular

---
+++ aprendiendo-angular/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { VideojuegoComponent } from './videojuego/videojuego.component';
+import { ZapatillasComponent } from './zapatillas/zapatillas.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    VideojuegoComponent,
+    ZapatillasComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ aprendiendo-angular/src/app/videojuego/videojuego.component.ts
@@ -0,0 +1,19 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'videojuego',
+    templateUrl:'./videojuego.component.html'
+})
+
+export class VideojuegoComponent{
+
+    public titulo:string;
+    public listado:string;
+
+
+    constructor(){
+        this.titulo="Componente de Videojuegos";
+        this.listado="Listado de los juegos mas populares";
+        console.log("Se ha cargado el componente: videojuego.component.ts");
+    }
+}
