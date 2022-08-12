f28b3c32e7bbfe06319f712ed59c756d067687a6
Jhonna Somarriba <jhonna.somarriba@gmail.com>
2022-08-11 22:25:59 -0600

    Eventos ciclos de vida

--- aprendiendo-angular/src/app/videojuego/videojuego.component.ts
+++ aprendiendo-angular/src/app/videojuego/videojuego.component.ts
@@ -1,11 +1,11 @@
-import { Component } from "@angular/core";
+import { Component,OnInit,DoCheck,OnDestroy } from "@angular/core";
 
 @Component({
     selector:'videojuego',
     templateUrl:'./videojuego.component.html'
 })
 
-export class VideojuegoComponent{
+export class VideojuegoComponent implements OnInit,DoCheck, OnDestroy{
 
     public titulo:string;
     public listado:string;
@@ -14,6 +14,21 @@ export class VideojuegoComponent{
     constructor(){
         this.titulo="Componente de Videojuegos";
         this.listado="Listado de los juegos mas populares";
+
         console.log("Se ha cargado el componente: videojuego.component.ts");
+    }
+
+    ngOnInit(): void {
+        console.log("OnInit Ejecutado");
+    }
+    ngDoCheck():void{
+        console.log("DoCheck Ejecutado");
+    }
+    ngOnDestroy(): void {
+        console.log("OnDestroy Ejecutado");
+    }
+
+    cambiarTitulo(){
+        this.titulo="Nuevo titulo del componente";
     }
 }
