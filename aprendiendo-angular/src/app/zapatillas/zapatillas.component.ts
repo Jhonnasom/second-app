5f301982d68701ae1de422c5f9f71bd2fe4d0729
unknown <jhonna.somarriba@gmail.com>
2022-08-18 13:36:44 -0600

    Directiva Structural ngFor

--- aprendiendo-angular/src/app/zapatillas/zapatillas.component.ts
+++ aprendiendo-angular/src/app/zapatillas/zapatillas.component.ts
@@ -9,17 +9,33 @@ import { Zapatilla } from "../models/zapatillas";
 export class ZapatillasComponent implements OnInit {
     public titulo:string="Componente de zapatillas";
     public zapatillas:Array<Zapatilla>;
+    public marcas:String[];
+    public color:string;
     
     constructor(){
+        this.color
+        this.marcas=new Array();
         this.zapatillas=[
             new Zapatilla('Reebook Classic','Rebook','Blanco',80,true),
             new Zapatilla('Nike Runner','Nike','Negras',60,true),
-            new Zapatilla('Adidas DC','Adidas','Gris',180,false)
+            new Zapatilla('Adidas DC','Adidas','Gris',180,true),
+            new Zapatilla('Reebok Spartan','Adidas','Gris',180,false)
 
         ];
     }
     ngOnInit(){
         console.log(this.zapatillas);
+        this.getMarcas();
+    }
+    getMarcas(){
+        this.zapatillas.forEach((zapatilla,index)=>{
 
+            if(this.marcas.indexOf(zapatilla.marca)<0){
+                this.marcas.push(zapatilla.marca);
+            }    
+            
+        });
+
+        console.log(this.marcas);
     }
 }
