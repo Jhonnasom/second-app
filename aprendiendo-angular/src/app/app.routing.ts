6ea4f00017d809d33a41ba3a5c5155938828417d
Jhonna Somarriba <jhonna.somarriba@gmail.com>
2022-08-30 21:05:44 -0600

    Configuracion del routing en angular y menu de nav

---
+++ aprendiendo-angular/src/app/app.routing.ts
@@ -0,0 +1,23 @@
+//Importar modulos del router de angular
+import { ModuleWithProviders } from "@angular/core";
+import {Routes, RouterModule}from "@angular/router";
+
+//Import components
+import { HomeComponent } from "./home/home.component";
+import { ZapatillasComponent } from "./zapatillas/zapatillas.component";
+import { VideojuegoComponent } from "./videojuego/videojuego.component";
+import { CursosComponent } from "./cursos/cursos.component";
+import { Route } from "@angular/router";
+
+//Array de configuracion de las rutas
+const appRoutes:Routes=[
+    {path:'',component:HomeComponent},
+    {path:'zapatillas',component:ZapatillasComponent},
+    {path:'videojuego',component:VideojuegoComponent},
+    {path:'cursos',component:CursosComponent},
+    {path:'**',component:HomeComponent}
+];
+
+//Exportar el modulo del router
+export const appRoutingProviders:any[]=[];
+export const routing:ModuleWithProviders<Route>=RouterModule.forRoot(appRoutes);
